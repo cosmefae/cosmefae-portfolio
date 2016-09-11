c91d345352eebde77931cd6b007b470c6727932f
Cosme Fae <cosmefm@gmail.com>
2016-09-11 13:53:57 -0300

    New content for Smart Fit case

--- work/smartfit/index.php
+++ work/smartfit/index.php
@@ -13,39 +13,180 @@
     <?php require_once("../../includes/header.php"); ?>
 
     <section id="content" class="post">
-      <figure class="cover-work">
-        <img src="cover-smartfit-logo.png">
-      </figure>
-
-      <div class="container">
-        <article>
-          <header>
+        <article class="no-sep">
+          <header class="container">
             <h2 class="title-main">Smart Fit</h2>
           </header>
 
           <div class="entry-content">
-            <p>A fitness mobile app proposal focused on motivating and engage people in exercises.
-              Smart Fit company is the largest network of Latin American academies.</p>
+            <section class="section">
+              <div class="container">
+                <article class="grid-center no-sep" data-bottom-top="opacity:0" data-center-top="opacity:1">
+                  <header>
+                    <h2>What is Smart Fit today</h2>
+                  </header>
+                  <p>Smart Fit company is the largest network of Latin American academies.
+                    This is a fitness mobile app proposal focused on motivating and engage people in exercises.
+                    </p>
+                  <p>
+                    Nowadays Smart Fit does not have a mobile app to connect her customers to the brand and their services.
+                  </p>
+                </article>
 
-            <p>After a talk with a friend about the gap in fitness's mobile app, I had the idea to create this
-              elegant and intuitive app. I based my strategy to make good gamification engine as a motivational
-              tool and an intuitive workout planner.</p>
-          </div>
+                <article class="grid-center" data-bottom-top="opacity:0" data-center-top="opacity:1">
+                  <header>
+                    <h2>How to get the first insight</h2>
+                  </header>
+                  <p>After a talk with a friend about the gap in fitness's mobile app, I had the idea to create this elegant
+                    and intuitive app. I based my strategy to make good gamification engine as a motivational tool and an
+                    intuitive workout planner.</p>
+                </article>
+              </div><!-- .container -->
+            </section>
 
-          <div class="gallery">
-            <img src="cover_scenario.jpg">
 
-            <iframe width="438" height="930" src="//invis.io/2F4VA6V6G" frameborder="0" allowfullscreen></iframe>
+            <hr>
 
-            <img src="cover_scenario_2.jpg">
-            <img src="smartfit_Perspective_MockUp.jpg">
+            <section class="section odd">
+              <article class="grid-left no-sep" data-bottom-top="opacity:0" data-center-top="opacity:1">
+                <header>
+                  <h2>The Concept</h2>
+                </header>
+                  <p>
+                    I spent a couple days planning the interface with some UX tools as wireframes, card sorting and
+                    usability tests. I used post-its to define some engaging features to the users spend more
+                    time using the app.
+                  </p>
+              </article>
 
-            <div class="bg-yellow">
-              <img src="cover_scenario_3.png">
-            </div>
-          </div>
+              <aside class="grid-right no-sep" data-bottom-top="transform: translate3d(500px, 0px, 0px);" data-center-top="transform: translate3d(0px, 0px, 0px);">
+                <div class="gall">
+                  <div><img src="<?=$url; ?>/resize.php?h=600&img=/portfolio/work/smartfit/cards_smartfit_01.jpg" alt=""></div>
+                  <div><img src="<?=$url; ?>/resize.php?h=600&img=/portfolio/work/smartfit/cards_smartfit_02.jpg" alt=""></div>
+                </div>
+              </aside>
+            </section>
+
+            <hr>
+
+
+            <section class="section">
+              <article class="grid-left">
+                <header>
+                  <h2>The Design</h2>
+                </header>
+                  <p>
+                    Social network connects as Facebook and Google Plus is a powerful tool to make app access easier for users.
+                    However, some users do not have a social network account so can connect via e-mail as well.
+                  </p>
+                  <p>
+                    Onboarding to share the amazing features that users will love.
+                  </p>
+              </article>
+              <aside class="grid-right no-sep">
+                <div class="gall">
+                  <div><img src="<?=$url; ?>/resize.php?w=375&img=/portfolio/work/smartfit/Intro 03.png" alt=""></div>
+                  <div><img src="<?=$url; ?>/resize.php?w=375&img=/portfolio/work/smartfit/Acesse.png" alt=""></div>
+                  <div><img src="<?=$url; ?>/resize.php?w=375&img=/portfolio/work/smartfit/Agenda Série 05 (actived).png" alt=""></div>
+                  <div><img src="<?=$url; ?>/resize.php?w=375&img=/portfolio/work/smartfit/Menu Opened.png" alt=""></div>
+                  <div><img src="<?=$url; ?>/resize.php?w=375&img=/portfolio/work/smartfit/Agenda Série 04.png" alt=""></div>
+                </div>
+              </aside>
+            </section>
+
+            <hr>
+
+
+            <section class="section odd">
+              <article class="grid-left">
+                <header>
+                  <h3>Motivating and notification cards</h3>
+                </header>
+                <p>
+                  I created some motivating and notification cards on the home screen app. I split into 3 simple categories:
+                </p>
+
+                <ul>
+                  <li><strong class="high">Gym notification</strong> Opening hours alerts (holidays, energy problems...), time to close
+                    its favourite gym, app new features, billing problems, special event (extra workout, running party,
+                    DJ at the gym...)</li>
+                  <li><strong class="high">Motivating content</strong> Quotes and videos from smart people to don't give up your workouts.</li>
+                  <li><strong class="high">Badges notification</strong> After reaching some activities along the time the users can get some
+                    motivating badges like 'You earned a medal level 3 "It doesn't matter if it's raining". Congrats!',
+                    'Non-stop 3 months. You look great as always' or 'You really deserve a white beard just
+                    to workout on 25th December'.</li>
+                </ul>
+              </article>
+              <aside class="grid-right no-sep">
+                <div><img src="<?=$url; ?>/resize.php?w=375&img=/portfolio/work/smartfit/dashboard_cards.png" alt=""></div>
+              </aside>
+            </section>
+
+
+            <hr>
+
+
+            <section class="section">
+              <article class="grid-right">
+                <header>
+                  <h2>Weather forecast</h2>
+                </header>
+                <p>The weather forecast for the day help you to choose the right clothes for you workout trainning.</p>
+              </article>
+              <aside class="grid-left no-sep">
+                  <figure><img src="<?=$url; ?>/resize.php?w=375&img=/portfolio/work/smartfit/weather_forecast.png" alt=""></figure>
+              </aside>
+            </section>
+
+            <hr>
+
+
+            <section class="section odd">
+              <div class="container">
+                <article class="grid-center">
+                  <header>
+                    <h2>Your coach can help you</h2>
+                  </header>
+                  <p>
+                    Nowadays, unfortunately, we can set an appointment with a coach just into the gym's reception. But with an
+                    intuitive workflow, you can choose the best time and workout planning with a gym coach inside the app.
+                  </p>
+                </article>
+                <aside class="no-sep show-images-2">
+                    <div><img src="<?=$url; ?>/resize.php?w=375&img=/portfolio/work/smartfit/Agenda Série 06.png" alt="">
+                      <img src="<?=$url; ?>/resize.php?w=375&img=/portfolio/work/smartfit/Agenda Série 08.png" alt=""></div>
+                </aside>
+              </div>
+            </section>
+
+            <hr>
+
+
+            <section class="section">
+              <div class="container">
+                <article class="grid-left">
+                  <header>
+                    <h2>Watch your training</h2>
+                  </header>
+                  <p>
+                    After your coach plan the workout, you will follow your daily activity. The user can use the integrated
+                    stopwatch to control the rest time easier than never.
+                  </p>
+                </article>
+                <aside class="grid-right no-sep">
+                  <div class="gall">
+                    <div><img src="<?=$url; ?>/resize.php?w=375&img=/portfolio/work/smartfit/Stopwatch.png" alt=""></div>
+                  </div>
+                </aside>
+              </div>
+            </section>
+
+          </div><!-- .entry-content -->
         </article>
-      </div>
+
+
+
+
     </section>
 
     <?php require_once("../../includes/footer.php"); ?>
